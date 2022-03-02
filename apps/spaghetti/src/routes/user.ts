5310a9e8fd81f167277cea36b3a1d955996f55f3
Tushar Chenan <tusky2002@gmail.com>
2022-03-02 12:40:12 +0530

    Added user routes, added admin auth middleware (#10)
    * Added user routes, added admin auth middleware
    
    * Fix Minor Privilege Issue in Routes
    
    Co-authored-by: Aditya Chopra <adeecc11@gmail.com>

--- apps/spaghetti/src/routes/user.ts
+++ apps/spaghetti/src/routes/user.ts
@@ -1,12 +1,114 @@
 import express from 'express';
-import { isLoggedIn } from '../utils/auth';
+import { isLoggedIn, isAdminLoggedIn } from '../utils/auth';
+import User from '../models/user';
 
 const router = express.Router();
 
-router.get('/', isLoggedIn, (req: express.Request, res: express.Response) => {
-  res.json({
+router.get('/me', isLoggedIn, (req: express.Request, res: express.Response) => {
+  return res.json({
     msg: req.user,
   });
 });
+
+router.put(
+  '/me',
+  isLoggedIn,
+  async (req: express.Request, res: express.Response) => {
+    const loggedInUser: any = req.user;
+    const id = loggedInUser.id;
+    const data = req.body;
+    try {
+      await User.findOneAndUpdate({ _id: id }, data);
+      return res.json(await User.findById(id));
+    } catch (e: any) {
+      return res.status(400).json({
+        err: 'User could not be updated',
+      });
+    }
+  }
+);
+
+router.delete(
+  '/me',
+  isLoggedIn,
+  async (req: express.Request, res: express.Response) => {
+    const loggedInUser: any = req.user;
+    const id = loggedInUser.id;
+    try {
+      await User.findOneAndDelete({ _id: id });
+      return res.json({
+        msg: 'User deleted',
+      });
+    } catch (e: any) {
+      return res.status(400).json({
+        err: 'User could not be deleted',
+      });
+    }
+  }
+);
+
+router.get(
+  '/',
+  isLoggedIn,
+  async (_req: express.Request, res: express.Response) => {
+    try {
+      return res.json(await User.find());
+    } catch (e: any) {
+      return res.status(400).json({
+        err: 'Unable to fetch all users',
+      });
+    }
+  }
+);
+
+router.get(
+  '/:userId',
+  isLoggedIn,
+  async (req: express.Request, res: express.Response) => {
+    const userId = req.params.userId;
+    try {
+      return res.json(await User.findById(userId));
+    } catch (e: any) {
+      return res.status(400).json({
+        err: 'Invalid userId',
+      });
+    }
+  }
+);
+
+router.put(
+  '/:userId',
+  isAdminLoggedIn,
+  async (req: express.Request, res: express.Response) => {
+    const userId = req.params.userId;
+    const data = req.body;
+    try {
+      await User.findOneAndUpdate({ _id: userId }, data);
+      return res.json(await User.findById(userId));
+    } catch (e: any) {
+      return res.status(400).json({
+        err: 'User could not be updated',
+      });
+    }
+  }
+);
+
+router.delete(
+  '/:userId',
+  isAdminLoggedIn,
+  async (req: express.Request, res: express.Response) => {
+    const userId = req.params.userId;
+    try {
+      await User.findOneAndDelete({ _id: userId });
+      return res.json({
+        msg: 'User deleted',
+      });
+    } catch (e: any) {
+      return res.status(400).json({
+        err: 'User could not be deleted',
+      });
+    }
+  }
+);
 
 export default router;
--- apps/spaghetti/src/utils/auth.ts
+++ apps/spaghetti/src/utils/auth.ts
@@ -5,9 +5,27 @@ export function isLoggedIn(
   res: express.Response,
   next: express.NextFunction
 ) {
-  if (req.isAuthenticated()) return next();
+  if (req.isAuthenticated()) {
+    return next();
+  }
 
   res.status(401).json({
     err: 'User not logged in',
+  });
+}
+
+export function isAdminLoggedIn(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  if (req.isAuthenticated()) {
+    const user: any = req.user;
+    if (user.role === 'admin') {
+      return next();
+    }
+  }
+  res.status(401).json({
+    err: 'Authorized user not logged in',
   });
 }
